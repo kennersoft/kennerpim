bd88e5376aeb547ff93f00d3891184c19c0c57a4
Roman Kovalyshyn <kovaroman@gmail.com>
2020-04-07 18:47:03 +0300

    Update copyright and change repo

--- client/modules/pim/src/views/modals/detail-full-form-disabled.js
+++ client/modules/pim/src/views/modals/detail-full-form-disabled.js
@@ -1,38 +1,29 @@
 /*
- * This file is part of EspoCRM and/or TreoCore.
- *
- * EspoCRM - Open Source CRM application.
- * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
- * Website: http://www.espocrm.com
- *
- * TreoCore is EspoCRM-based Open Source application.
- * Copyright (C) 2017-2019 TreoLabs GmbH
- * Website: https://treolabs.com
- *
- * KennerPIM is Pim-based Open Source application.
- * Copyright (C) 2020 KenerSoft Service GmbH
+ * Pim
+ * Free Extension
+ * Copyright (c) 2020 Kenner Soft Service GmbH
  * Website: https://kennersoft.de
  *
- * TreoCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TreoCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
- * and "TreoCore" word.
+ * these Appropriate Legal Notices must retain the display of the "KennerPIM"
+ * word.
  */
 
 Espo.define('pim:views/modals/detail-full-form-disabled', 'views/modals/detail',
